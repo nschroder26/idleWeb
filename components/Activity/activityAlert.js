48290ada788f55ac2f5d57d7af46caf1770d6705
Nicholas Schroder <nrs4492@hotmail.com>
2022-12-05 14:59:43 -0500

    add alert component

--- components/Activity/activityAlert.js
+++ components/Activity/activityAlert.js
@@ -1,10 +1,28 @@
-import { Card } from "react-bootstrap";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import classes from "./activityAlert.module.css";
 
 const ActivityAlert = () => {
   return (
     <Card className={classes.alertCard}>
-      <Card.Text className={classes.alertText}>This is an alert</Card.Text>
+      <Container>
+        <Row>
+          <Col className={classes.alertCol}>
+            <Card.Text className={classes.alertText}>
+              This is an alert
+            </Card.Text>
+          </Col>
+          <Col md="auto">
+            <Button
+              variant="outline-dark"
+              size="sm"
+              className={classes.alertDismiss}
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </Button>
+          </Col>
+        </Row>
+      </Container>
     </Card>
   );
 };
